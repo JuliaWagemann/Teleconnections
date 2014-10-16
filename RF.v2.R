80a950f8bc1762e80ea710c71dcd0e280bb59516
fcecinati <fcecinati@gmail.com>
2014-10-16 08:41:16 +0200

    Updated working version of the first draft for random forest algorithm

---
+++ RF.v2.R
@@ -0,0 +1,149 @@
+library(randomForest)
+library(ncdf)
+library(RNetCDF)
+library(ncdf4)
+library(fields)
+library(raster)
+
+#########################################################################
+# Section for individual adjustment
+# Please specify all variables in this section individually for every single parameter
+#########################################################################
+# define the number of trees for the random forest
+NTree <- 500
+
+#BE SURE THAT ALL THE FOLDERS CONTAIN THE SAME YEARS!!!
+#How many years are contained in each folder?
+Nyears <- 35
+
+#define the paths of the predictors:
+path1 <- "//TEC-JULIA-FRA/Users/julia_francesca/Documents/Data/2_Anomalies/ERA_Interim/monthly/1_Geopotential_500/3deg"
+path2 <- "//TEC-JULIA-FRA/Users/julia_francesca/Documents/Data/2_Anomalies/ERA_Interim/monthly/2_SLP/3deg"
+path3 <- "//TEC-JULIA-FRA/Users/julia_francesca/Documents/Data/2_Anomalies/ERA_Interim/monthly/3_T2m/3deg"
+
+#define the path of the variable to be predicted:
+path4 <- "//TEC-JULIA-FRA/Users/julia_francesca/Documents/Data/2_Anomalies/ERA_Interim/monthly/4_TP/3deg"
+
+# what are the dimension of the grid?
+geoLat <- 60
+geoLon <- 120
+
+OutputVar <- "RF_TP"
+########################################################################
+fileList1 <- list.files(path1, pattern=".nc")
+fileList2 <- list.files(path2, pattern=".nc")
+fileList3 <- list.files(path3, pattern=".nc")
+fileList4 <- list.files(path4, pattern=".nc")
+
+nrOfTimeSteps <- Nyears*12
+nrOfPixels <- 14*19
+
+vector1 <- vector(mode = "numeric", length = nrOfTimeSteps*nrOfPixels)
+vector2 <- vector(mode = "numeric", length = nrOfTimeSteps*nrOfPixels)
+vector3 <- vector(mode = "numeric", length = nrOfTimeSteps*nrOfPixels)
+vector4 <- vector(mode = "numeric", length = nrOfTimeSteps*nrOfPixels)
+
+#predictor 1
+setwd(path1)
+dataMatrix <- matrix(0, nrOfTimeSteps, nrOfPixels)
+t <- 1  #initialize a count variable
+for(m in fileList1){
+
+        GP_anomaly <- open.ncdf(m, write=FALSE)
+        GP_var <- get.var.ncdf(GP_anomaly, "GP500_anomaly")
+        for(z in 1:12){
+                tempRaster <- GP_var[,,z]
+                tempRaster[tempRaster=="NaN"] <- NA
+                tempRaster <- raster(t(tempRaster), xmn=-180, xmx=180, ymn=-90, ymx=90)
+                tempRaster <- crop(tempRaster, extent(c(-12, 45, 30, 72)))
+                tempRaster <- as.matrix(tempRaster)
+                dataMatrix[t,] <- tempRaster
+                t <- t+1 
+        }
+}
+vector1 <- as.vector(mode="numeric", dataMatrix)
+
+#predictor 2
+setwd(path2)
+dataMatrix <- matrix(0, nrOfTimeSteps, nrOfPixels)
+t <- 1  #initialize a count variable
+for(m in fileList2){
+        
+        GP_anomaly <- open.ncdf(m, write=FALSE)
+        GP_var <- get.var.ncdf(GP_anomaly, "SLP_anomaly")
+        for(z in 1:12){
+                tempRaster <- GP_var[,,z]
+                tempRaster[tempRaster=="NaN"] <- NA
+                tempRaster <- raster(t(tempRaster), xmn=-180, xmx=180, ymn=-90, ymx=90)
+                tempRaster <- crop(tempRaster, extent(c(-12, 45, 30, 72)))
+                tempRaster <- as.matrix(tempRaster)
+                dataMatrix[t,] <- tempRaster
+                t <- t+1 
+        }
+}
+vector2 <- as.vector(mode="numeric", dataMatrix)
+
+#predictor 3
+setwd(path3)
+dataMatrix <- matrix(0, nrOfTimeSteps, nrOfPixels)
+t <- 1  #initialize a count variable
+for(m in fileList3){
+        
+        GP_anomaly <- open.ncdf(m, write=FALSE)
+        GP_var <- get.var.ncdf(GP_anomaly, "T2m_anomaly")
+        for(z in 1:12){
+                tempRaster <- GP_var[,,z]
+                tempRaster[tempRaster=="NaN"] <- NA
+                tempRaster <- raster(t(tempRaster), xmn=-180, xmx=180, ymn=-90, ymx=90)
+                tempRaster <- crop(tempRaster, extent(c(-12, 45, 30, 72)))
+                tempRaster <- as.matrix(tempRaster)
+                dataMatrix[t,] <- tempRaster
+                t <- t+1 
+        }
+}
+vector3 <- as.vector(mode="numeric", dataMatrix)
+
+#output
+setwd(path4)
+dataMatrix <- matrix(0, nrOfTimeSteps, nrOfPixels)
+t <- 1  #initialize a count variable
+for(m in fileList4){
+        
+        GP_anomaly <- open.ncdf(m, write=FALSE)
+        GP_var <- get.var.ncdf(GP_anomaly, "TP_anomaly")
+        for(z in 1:12){
+                tempRaster <- GP_var[,,z]
+                tempRaster[tempRaster=="NaN"] <- NA
+                tempRaster <- raster(t(tempRaster), xmn=-180, xmx=180, ymn=-90, ymx=90)
+                tempRaster <- crop(tempRaster, extent(c(-12, 45, 30, 72)))
+                tempRaster <- as.matrix(tempRaster)
+                dataMatrix[t,] <- tempRaster
+                t <- t+1 
+        }
+}
+vector4 <- as.vector(mode="numeric", dataMatrix)
+
+set.seed(20)
+
+DF <- data.frame(vector1, vector2, vector3)
+
+RF <- randomForest(x=DF, y=vector4, ntree=NTree)
+
+pred_m <- matrix(RF$predicted, nrOfPixels, nrOfTimeSteps)
+st <- stack()
+for (i in 1:nrOfTimeSteps){
+        layer <- matrix(pred_m[,i], 14, 19)
+        rast <- raster(layer)
+        st <- addLayer(st, rast)
+}
+prediction <- st
+
+y_m <- matrix(RF$y, nrOfPixels, nrOfTimeSteps)
+st <- stack()
+for (i in 1:nrOfTimeSteps){
+        layer <- matrix(pred_m[,i], 14, 19)
+        rast <- raster(layer)
+        st <- addLayer(st, rast)
+}
+
+y <- st
